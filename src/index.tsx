09f0aed4ca3579c8be6562d26ff705c9bc25b4ef
superdev905 <superdev905@gmail.com>
2016-10-31 13:58:27 -0700

    App is finally working with few tslint warnings.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-import { Hello } from './components/Hello';
+import { Hello } from './components/HelloWorld/HelloWorld';
 
 ReactDOM.render(
-    <Hello compiler='TypeScript' framework='React' />,
+    <Hello description='Hello World from web-library-build' />,
     document.getElementById('example')
 );
